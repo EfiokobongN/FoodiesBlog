697f96d0273d09447ad2e618f27cd758525f7d09
EfiokobongN <mailpreciousnelson@gmail.com>
2024-08-27 12:23:48 +0000

    alx project

--- api/index.js
+++ api/index.js
@@ -90,38 +90,45 @@ app.post('/login', async (req,res) => {
     });
   
   });
+//Edit Post 
 
-  //edit
-  app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
     let newPath = null;
     if (req.file) {
-      const {originalname,path} = req.file;
+      const { originalname, path } = req.file;
       const parts = originalname.split('.');
       const ext = parts[parts.length - 1];
-      newPath = path+'.'+ext;
+      newPath = path + '.' + ext;
       fs.renameSync(path, newPath);
     }
   
-    const {token} = req.cookies;
-    jwt.verify(token, secret, {}, async (err,info) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
       if (err) throw err;
-      const {id,title,summary,content} = req.body;
+  
+      const { id, title, summary, content } = req.body;
       const postDoc = await Post.findById(id);
+  
+      if (!postDoc) {
+        return res.status(404).json('Post not found');
+      }
+  
       const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
       if (!isAuthor) {
-        return res.status(400).json('you are not the author');
+        return res.status(403).json('You are not the author');
       }
-      await postDoc.update({
-        title,
-        summary,
-        content,
-        file: newPath ? newPath : postDoc.file,
-      });
   
+
+      postDoc.title = title;
+      postDoc.summary = summary;
+      postDoc.content = content;
+      postDoc.file = newPath ? newPath : postDoc.file;
+  
+      await postDoc.save();
       res.json(postDoc);
     });
-  
   });
+  
   //getting posts
 
   app.get('/post', async (req,res) => {
